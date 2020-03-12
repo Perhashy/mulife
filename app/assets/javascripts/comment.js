25c945de027a60467777a52d1a70f9bd14fd93f1
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-03-12 19:07:01 +0900

    good_coding03

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,42 +1,44 @@
-$(function(){
-  function buildHTML(comment){
-    var html = `<div class="comment">
-                  <div class="comment-user">
-                    <img class="comment-user__image" src=${comment.user_image}>
-                      <div class="comment-user__name">
-                        <a class="comment-user__name" href=/users/${comment.user_id}>${comment.user_name}</a>
-                          ：
-                      </div>
-                      <div class="comment-user__message">
-                        <p>${comment.text}</p>
-                      </div>
-                  </div>
-                  <div class="comment-edit">
-                    <a rel="nofollow" data-method="delete" ${content_comment_path(@content, comment)}>削除</a>
-                  </div>
-                </div>`
-    return html;
-  }
-  $('#new_comment').on('submit', function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.comments').append(html);
-      $('.comment-input').val('');
-      $('.comment-submit').prop('disabled', false);
-    })
-    .fail(function(){
-      alert('error');
-    })
-  })
-});
+// $(function(){
+//   function buildHTML(comment){
+//     var html = `
+//                   <div class="comment" data-message-id=${comment.id}>
+//                     <div class="comment-user">
+//                       <img class="comment-user__image" src=${comment.user_image}>
+//                         <div class="comment-user__name">
+//                           <a class="comment-user__name" href=/users/${comment.user_id}>${comment.user_name}</a>
+//                             ：
+//                         </div>
+//                         <div class="comment-user__message">
+//                           <p>${comment.text}</p>
+//                         </div>
+//                     </div>
+//                     <div class="comment-edit">
+//                       <a rel="nofollow" data-method="delete" href=/contents/${comment.content_id}/comments/${comment.id}>削除</a>
+//                     </div>
+//                   </div>
+//                 `
+//     return html;
+//   }
+//   $('#new_comment').on('submit', function(e){
+//     e.preventDefault();
+//     var formData = new FormData(this);
+//     var url = $(this).attr('action');
+//     $.ajax({
+//       url: url,
+//       type: "POST",
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+//     })
+//     .done(function(data){
+//       var html = buildHTML(data);
+//       $('.comment-list').append(html);
+//       $('.comment-input').val('');
+//       $('.comment-submit').prop('disabled', false);
+//     })
+//     .fail(function(){
+//       alert('error');
+//     })
+//   })
+// });
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,10 +2,11 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.create(comment_params)
-    respond_to do |format|
-      format.html { redirect_to content_path(params[:content_id])  }
-      format.json
-    end
+    redirect_to content_path(params[:content_id])
+    # respond_to do |format|
+    #   format.html { redirect_to content_path(params[:content_id])  }
+    #   format.json
+    # end
   end
 
   def destroy
