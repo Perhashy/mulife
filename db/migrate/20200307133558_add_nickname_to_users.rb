852a349960d3f8bcc7c647c4b1e6ccca0a702137
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-04-12 17:05:28 +0900

    DB再作成

--- db/migrate/20200307133558_add_nickname_to_users.rb
+++ db/migrate/20200307133558_add_nickname_to_users.rb
@@ -1,5 +1,8 @@
 class AddNicknameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :nickname, :string
+    add_column :users, :nickname, :string, null: false
+    add_column :users, :part_id, :integer, null: false
+    add_column :users, :message, :string
+    add_column :users, :image, :string
   end
 end
--- db/migrate/20200309082714_add_message_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddMessageToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :part, :string
-    add_column :users, :message, :text
-  end
-end
