62f0450a8b2125f287015544f938d9494c665af7
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-04-12 17:27:18 +0900

    モデルに記述

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,5 +1,11 @@
 class Part < ActiveHash::Base
   self.data = [
-    {id: 1, name: '送料込み（出品者負担）'},{id: 2, name:'着払い（購入者負担）'}
+    {id: 1, name: 'ボーカル'},{id: 2, name:'ギター'},
+    {id: 3, name: 'ベース'},{id: 4, name: 'ドラム'},
+    {id: 5, name: 'キーボード'},{id: 6, name: 'ギター&ボーカル'},
+    {id: 7, name: 'ベース&ボーカル'},{id: 8, name: 'パーカッション'},
+    {id: 9, name: 'ピアノ'},{id: 10, name: 'バイオリン'},
+    {id: 11, name: 'DJ'},{id: 12, name: '作曲'},
+    {id: 13, name: 'その他'}
   ]
 end
