5915c4027029fb5516fc423c4ed91f5d41550b35
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-04-14 20:08:27 +0900

    タグ検索

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,7 +1,6 @@
 class TagsController < ApplicationController
   def index
-    if params[:tag_name]
-      @contents = Content.tagged_with("#{params[:tag_name]}").page(params[:page]).per(5)
-    end
+    @tags = Content.tag_counts_on(:tags).order('count DESC')
+    @contents = Content.tagged_with("#{params[:tag_name]}").page(params[:page]).per(5)
   end
 end
