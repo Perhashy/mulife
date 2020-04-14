100915cbf3ae6b2006cc2867f59b95f49766680f
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-04-14 20:28:28 +0900

    タグ並び替え名前順

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,6 @@
 class TagsController < ApplicationController
   def index
-    @tags = Content.tag_counts_on(:tags).order('count DESC')
-    @contents = Content.tagged_with("#{params[:tag_name]}").page(params[:page]).per(5)
+    @tags = Content.tag_counts_on(:tags).order('name DESC')
+    @contents = Content.tagged_with("#{params[:tag_name]}").order("created_at DESC").page(params[:page]).per(5)
   end
 end
